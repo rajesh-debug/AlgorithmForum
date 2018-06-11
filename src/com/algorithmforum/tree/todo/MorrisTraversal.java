311385e91e1f7b8db181ce3f492a64e6e1b9271b
rajesh.dixit <rajesh.dixit@192.168.1.8>
2018-06-11 22:11:24 +0530

    Comment in the code for Morris traversal.

--- src/com/algorithmforum/tree/todo/MorrisTraversal.java
+++ src/com/algorithmforum/tree/todo/MorrisTraversal.java
@@ -1,5 +1,26 @@
 package com.algorithmforum.tree.todo;
 
+/**
+ *
+ * Using Morris Traversal, we can traverse the tree without using stack and recursion.
+ * The idea of Morris Traversal is based on Threaded Binary Tree.
+ * 
+ * In this traversal, we first create links to Inorder successor and print the data using these links,
+ * and finally revert the changes to restore original tree.
+ *
+ * 1. Initialize current as root 
+ * 2. While current is not NULL 
+ * 		If current does not have left child
+ * 			a) Print current’s data
+ * 			b) Go to the right, i.e., current = current->right
+ * 		Else
+ * 			a) Make current as right child of the rightmost node in current's left subtree
+ * 			b) Go to this left child, i.e., current = current->left
+ * https://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion-and-without-stack/
+ * 
+ * @author rajesh.dixit
+ *
+ */
 public class MorrisTraversal {
 
 	private static class Node {
