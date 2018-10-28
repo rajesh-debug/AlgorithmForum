fc72aaab02345c9bf0f7f4668785ed104d284f49
rajesh-debug <rkdixit3@gmail.com>
2018-10-28 23:25:21 +0530

    Post order traversal of tree.

---
+++ src/com/algorithmforum/tree/CreatePostOrder.java
@@ -0,0 +1,50 @@
+package com.algorithmforum.tree;
+
+
+public class CreatePostOrder {
+
+    private static int preIdx = 0;
+
+    private static void printPost(int[] in, int[] pre, int start, int end) {
+
+        if (start > end) {
+            return;
+        }
+
+        int inIdx = search(in, start, end, pre[preIdx++]);
+
+        printPost(in, pre, start, inIdx - 1);
+
+        printPost(in, pre, inIdx + 1, end);
+
+        System.out.print(in[inIdx] + " ");
+    }
+
+    /**
+     * This method returns the index in the inorder traversal.
+     * 
+     * @param in
+     * @param start
+     * @param end
+     * @param val
+     * @return return index in order traversal.
+     */
+    private static int search(int[] in, int start, int end, int val) {
+        int j = 0;
+        for (j = start; j < end; j++) {
+            if (in[j] == val) {
+                break;
+            }
+        }
+        return j;
+    }
+
+    public static void main(String ars[]) {
+        int in[] = { 4, 2, 5, 1, 3, 6 };
+        int pre[] = { 1, 2, 4, 5, 3, 6 };
+        int len = in.length;
+        printPost(in, pre, 0, len - 1);
+    }
+
+
+}
